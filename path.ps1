4af3cfb28b974e3ff34774eee572fc694c7e47a5
Th1nThr3ad <126841263+Th1nThr3ad@users.noreply.github.com>
2023-03-02 20:54:27 -0600

    Add files via upload

---
+++ path.ps1
@@ -0,0 +1,20 @@
+﻿$names = dir -recurse -file *.exe 'C:\Program Files\PowerToys' | wsl grep -o C:.* | sort -Unique | wsl grep -v -e '^[[:space:]]*$'
+
+
+
+For( $i=0; $i -le $names.length; $i++){
+    
+    $addPath = $names[$i] 
+   
+    if (Test-Path $addPath){
+        $regexAddPath = [regex]::Escape($addPath)
+        #$env:Path -split ';' | Where-Object {$_ -notMatch "^$regexAddPath\\?"}
+        $arrPath = $env:Path -split ';' | Where-Object {$_ -notMatch "^$regexAddPath\\?"}
+        $env:Path = ($arrPath + $addPath) -join ';'
+        echo $addPath
+        echo  "sucessfully added "
+    } else {
+        Throw "'$addPath' is not a valid path."
+    }
+   
+}
